59b00ab8e56770a7b4069969ac0a23fb0bbd1599
Arif Matubber <am@hoxro.com>
2021-12-16 01:56:13 +0600

    s

--- src/app/single-page/components/portfolios/portfolios.component.ts
+++ src/app/single-page/components/portfolios/portfolios.component.ts
@@ -8,35 +8,41 @@ import { Component, OnInit } from '@angular/core';
 export class PortfoliosComponent implements OnInit {
   features: any[] = [
     {
-      image: '../../../../assets/portf/01.png',
-      title: 'UI/UX Design',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis.'
+      image: '../../../../assets/images/kanban.png',
+      title: 'Web App',
+      description: 'Kanban board for managing tasks.',
+      urls: 'https://kanban-fire-workshop-8ca5d.web.app/'
     },
     {
-      image: '../../../../assets/portf/02.png',
-      title: 'UI/UX Design',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis.'
+      image: '../../../../assets/images/ds.png',
+      title: 'Web App',
+      description: 'Designe system for fast productivity',
+      urls: 'https://designsystemhx.web.app/icons'
     },
     {
       image: '../../../../assets/portf/03.png',
       title: 'UI/UX Design',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis.'
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis.',
+      urls: ''
     },
     {
       image: '../../../../assets/portf/04.png',
       title: 'UI/UX Design',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis.'
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis.',
+      urls: ''
     },
     {
       image: '../../../../assets/portf/05.png',
       title: 'UI/UX Design',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis.'
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis.',
+      urls: ''
     },
    
     {
       image: '../../../../assets/portf/06.png',
       title: 'UI/UX Design',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis.'
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla venenatis.',
+      urls: ''
     },
    
   ]
